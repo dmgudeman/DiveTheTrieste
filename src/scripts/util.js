ddca71fa33ed139bc57870339d455f2b48c0ee26
David M Gudeman <davidmgudeman@gmail.com>
2023-04-06 14:21:12 -0700

    remove console

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -9,7 +9,7 @@ export function getCursorPosition(canvas, event) {
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
-  console.log("x: " + x + " y: " + y);
+//   console.log("x: " + x + " y: " + y);
 }
 
 export function showCanvas1() {
