c1259f009f840c226f1b0002466bcada2159d3d9
David Gudeman <davidmgudeman@gmail.com>
2023-05-27 19:31:03 -0700

    ditto

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -1,6 +1,6 @@
-import Sub from "./sub";
-import Ocean from "./ocean";
-import { addAndStartMessAnimation, removeMessageElement, stopMessAnimation } from "./edMessage";
+// import Sub from "./sub";
+// import Ocean from "./ocean";
+// import { addAndStartMessAnimation, removeMessageElement, stopMessAnimation } from "./edMessage";
 import { WIDTH, HEIGHT } from "../index";
 import { stopMessageAnimation } from "./constants";
 import { getMessage } from "./provideMessage";
@@ -13,7 +13,6 @@ export function getCursorPosition(canvas, event) {
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
-//   console.log("x: " + x + " y: " + y);
 }
 
 
@@ -21,10 +20,10 @@ export function getCursorPosition(canvas, event) {
 // canvas2 is the opening page
 // canvas3 is the cockpit
 const canvas1 = document.getElementById("canvas1");
-const ctx1 = canvas1.getContext("2d");
+// const ctx1 = canvas1.getContext("2d");
 const canvas2 = document.getElementById("canvas2");
 const canvas3 = document.getElementById("canvas3");
-const ctx3 = canvas1.getContext("2d");
+// const ctx3 = canvas1.getContext("2d");
 const gauge = document.querySelector('.gaugeContainer');
 const trieste3Container = document.getElementById("trieste3Container");
 const homeButton = document.getElementById("homeButton")
@@ -72,8 +71,6 @@ export function showCanvas1() { //the ocean
 }
 
 export function showCanvas2() { //opening page
-  // clearInterval(messageInterval);
-  // stopMessAnimation();
   stopMessageAnimation.flag = false;
 
   canvas1.style.display = "none";
@@ -89,9 +86,7 @@ export function showCanvas2() { //opening page
   homeButton.classList.remove("can1home");
   homeButton.classList.add("can2home");
   homeButton.classList.remove("can3home"); 
-  edMessage.classList.add("hideEdMessage");
-  
-  
+  edMessage.classList.add("hideEdMessage");  
 }
 
 export function showCanvas3() {  //cockpit
@@ -110,18 +105,6 @@ export function showCanvas3() {  //cockpit
   homeButton.classList.remove("can2home");
   homeButton.classList.add("can3home"); 
   edMessage.classList.add("hideEdMessage");
- 
-  // const bubblesContainer = document.querySelector(".bubblesContainer");
-  // const bubbles = bubblesContainer.getElementsByClassName('bubble');
-  // for (let i = 0; i < bubbles.length; i++) {
-  //   const bubble = bubbles[i]; 
-  //   bubble.style.animation = 'none';
-  //   bubble.style.display = 'none';
-  // }
-  // bubblesContainer.classList.add('hide'); 
-  // clearInterval(messageInterval)
-  // stopMessAnimation();
-  // removeMessageElement();
   
 }
 
