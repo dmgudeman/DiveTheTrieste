717593f1d81962cff953d83fbc4b4e10971fe414
David Gudeman <davidmgudeman@gmail.com>
2023-06-17 11:36:44 -0700

    the onClick instrucitons set is now responsively placed

--- src/scripts/canvas2Helpers.ts
+++ src/scripts/canvas2Helpers.ts
@@ -6,31 +6,42 @@ export const drawCanvas2 = (canvas2, ctx2, backgroundImage, isInstructions1Visib
     let instructions2 = new Image();
     instructions2.src = "src/assets/dtt9.png";
 
-    // Encapsulate the drawing logic into a separate function
-    // Encapsulate the drawing logic into a separate function
+     // resize responsive
+     let docWidth = canvas2.width * 0.15;
+     let bannerHeight = canvas2.height * 0.065;
+     let docX = (canvas2.width - docWidth) / 6;
+     let bannerY = bannerHeight * 0.6; 
+
+      // Instruction set
+      let instructionsWidth = canvas2.width * 0.15;
+      let instructionsHeight = canvas2.width * 0.18; 
+      let instructionsX = (canvas2.width - docWidth) / 6;
+      let instructionsY = (bannerHeight + bannerY);
+
+
     const draw = () => {
-        ctx2.clearRect(0, 0, canvas2.width, canvas2.height); // Clear the canvas before redrawing
+        ctx2.clearRect(0, 0, canvas2.width, canvas2.height); 
         ctx2.drawImage(backgroundImage, 0, 0, canvas2.width, canvas2.height);
 
-        // Define position and size as percentages of the canvas dimensions
-        let docWidth = canvas2.width * 0.15;
-        let bannerHeight = canvas2.height * 0.065;
-        let bannerX = (canvas2.width - docWidth) / 6;
-        let bannerY = bannerHeight * 0.6; // Put the banner at the top
+        // // resize responsive
+        // let docWidth = canvas2.width * 0.15;
+        // let bannerHeight = canvas2.height * 0.065;
+        // let docX = (canvas2.width - docWidth) / 6;
+        // let bannerY = bannerHeight * 0.6; 
 
         ctx2.fillStyle = "#fff";
-        ctx2.fillRect(bannerX, bannerY, docWidth, bannerHeight);
+        ctx2.fillRect(docX, bannerY, docWidth, bannerHeight);
         ctx2.fillStyle = "#4CAF50";
         ctx2.font = "bold 50px Arial";
-        ctx2.fillText("DIVE THE TRIESTE!", bannerX + docWidth * 0.1, bannerY + bannerHeight * 0.7, docWidth * 0.8);
+        ctx2.fillText("DIVE THE TRIESTE!", docX + docWidth * 0.1, bannerY + bannerHeight * 0.7, docWidth * 0.8); // last param is max length of string
 
-        // // Define the size of the instructions
-        let instructionsWidth = canvas2.width * 0.15;
-        let instructionsHeight = canvas2.width * 0.18; // Make the instructions square
-        let instructionsX = (canvas2.width - docWidth) / 6;
-        let instructionsY = (bannerHeight + bannerY);
+        // // Instruction set
+        // let instructionsWidth = canvas2.width * 0.15;
+        // let instructionsHeight = canvas2.width * 0.18; 
+        // let instructionsX = (canvas2.width - docWidth) / 6;
+        // let instructionsY = (bannerHeight + bannerY);
 
-        if (isInstructions1Visible) {
+        if (isInstructions1Visible) {  // the minus 1 below is so there is no line between banner and document
             ctx2.drawImage(instructions1, instructionsX, instructionsY - 1, instructionsWidth, instructionsHeight);
         } else {
             ctx2.drawImage(instructions2, instructionsX, instructionsY - 1, instructionsWidth, instructionsHeight);
@@ -43,13 +54,12 @@ export const drawCanvas2 = (canvas2, ctx2, backgroundImage, isInstructions1Visib
     // Call the draw function on window resize
     window.addEventListener("resize", draw);
 
-    // The onclick event handler does not need to be inside the draw function
     canvas2.onclick = function (e) {
         let rect = canvas2.getBoundingClientRect();
         let x = e.clientX - rect.left;
         let y = e.clientY - rect.top;
 
-        if (x > 450 && x < 450 + 600 && y > 200 && y < 200 + 600) {
+        if (x > docX && x < docX + docWidth && y > bannerY && y < bannerY + instructionsHeight) {
             isInstructions1Visible = !isInstructions1Visible; // Toggle the flag
             draw(); // Redraw the canvas with the updated isInstructions1Visible flag
         }
