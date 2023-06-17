31f3f16a09dbea5cdc9c41c9553b8b7519d54195
David Gudeman <davidmgudeman@gmail.com>
2023-06-17 04:01:24 -0700

    remove the firebase initializer

--- src/index.ts
+++ src/index.ts
@@ -14,23 +14,7 @@ import CalcPosition from "./scripts/calcPosition";
 import InitialValues from "./scripts/initialValues";
 
 const initialValues = InitialValues.getInstance();
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyClP4_2l-rcLjMNBt8Bjo1L-FQfrdAgKbQ",
-//   authDomain: "dive-the-trieste.firebaseapp.com",
-//   projectId: "dive-the-trieste",
-//   storageBucket: "dive-the-trieste.appspot.com",
-//   messagingSenderId: "1060017973252",
-//   appId: "1:1060017973252:web:a6e9a823c8b574cb09ad51"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
 
 export let WIDTH = visualViewport.width * 2; // width of canvases
 export let HEIGHT = visualViewport.height * 2.05; // height of canvases
