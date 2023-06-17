36f8bc808af211da9be6f6d45af963317ed35211
David Gudeman <davidmgudeman@gmail.com>
2023-06-17 03:16:41 -0700

    delete firebase cache

--- src/index.ts
+++ src/index.ts
@@ -20,17 +20,17 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyClP4_2l-rcLjMNBt8Bjo1L-FQfrdAgKbQ",
-  authDomain: "dive-the-trieste.firebaseapp.com",
-  projectId: "dive-the-trieste",
-  storageBucket: "dive-the-trieste.appspot.com",
-  messagingSenderId: "1060017973252",
-  appId: "1:1060017973252:web:a6e9a823c8b574cb09ad51"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyClP4_2l-rcLjMNBt8Bjo1L-FQfrdAgKbQ",
+//   authDomain: "dive-the-trieste.firebaseapp.com",
+//   projectId: "dive-the-trieste",
+//   storageBucket: "dive-the-trieste.appspot.com",
+//   messagingSenderId: "1060017973252",
+//   appId: "1:1060017973252:web:a6e9a823c8b574cb09ad51"
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
 export let WIDTH = visualViewport.width * 2; // width of canvases
 export let HEIGHT = visualViewport.height * 2.05; // height of canvases
