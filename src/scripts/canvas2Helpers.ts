f2f3837d1507e286b18fba7a55940ab557916de4
David Gudeman <davidmgudeman@gmail.com>
2023-06-17 11:10:22 -0700

    adjust the instructions

--- src/scripts/canvas2Helpers.ts
+++ src/scripts/canvas2Helpers.ts
@@ -7,21 +7,33 @@ export const drawCanvas2 = (canvas2, ctx2, backgroundImage, isInstructions1Visib
     instructions2.src = "src/assets/dtt9.png";
 
     // Encapsulate the drawing logic into a separate function
+    // Encapsulate the drawing logic into a separate function
     const draw = () => {
         ctx2.clearRect(0, 0, canvas2.width, canvas2.height); // Clear the canvas before redrawing
         ctx2.drawImage(backgroundImage, 0, 0, canvas2.width, canvas2.height);
 
-        let banner = { x: 450, y: 100, width: 600, height: 100 };
+        // Define position and size as percentages of the canvas dimensions
+        let bannerWidth = canvas2.width * 0.15;
+        let bannerHeight = canvas2.height * 0.065;
+        let bannerX = (canvas2.width - bannerWidth) / 6;
+        let bannerY = bannerHeight * 0.6; // Put the banner at the top
+
         ctx2.fillStyle = "#fff";
-        ctx2.fillRect(banner.x, banner.y, banner.width, banner.height);
+        ctx2.fillRect(bannerX, bannerY, bannerWidth, bannerHeight);
         ctx2.fillStyle = "#4CAF50";
         ctx2.font = "bold 50px Arial";
-        ctx2.fillText("DIVE THE TRIESTE!", 520, 175);
+        ctx2.fillText("DIVE THE TRIESTE!", bannerX + bannerWidth * 0.1, bannerY + bannerHeight * 0.7);
+
+        // // Define the size of the instructions
+        let instructionsWidth = canvas2.width * 0.5;
+        let instructionsHeight = instructionsWidth; // Make the instructions square
+        let instructionsX = (canvas2.width - instructionsWidth) / 2;
+        let instructionsY = (canvas2.height - instructionsHeight) / 2;
 
         if (isInstructions1Visible) {
-            ctx2.drawImage(instructions1, 450, 200, 600, 600);
+            ctx2.drawImage(instructions1, instructionsX, instructionsY, instructionsWidth, instructionsHeight);
         } else {
-            ctx2.drawImage(instructions2, 450, 200, 600, 600);
+            ctx2.drawImage(instructions2, instructionsX, instructionsY, instructionsWidth, instructionsHeight);
         }
     };
 
